d7a3344ef8d81d97a5e97501ec2f5d1ed941ee74
Vorak Tong <viraktong197@gmail.com>
2026-04-16 14:28:30 +0700

    Create Repo and Mock Repo for station

--- lib/data/repositories/station/station_repository.dart
+++ lib/data/repositories/station/station_repository.dart
@@ -1,1 +1,7 @@
-abstract class StationRepository {}
+import 'package:g4_t1_velo_app/model/station.dart';
+
+abstract class StationRepository {
+  Future<List<Station>> getStations();
+
+  Future<Station?> getStationById(String id);
+}
