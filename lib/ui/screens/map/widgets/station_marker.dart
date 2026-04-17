8ee233abb5c3a5b9128c4056317c20aad27fceec
Zorng <ieangzorngl@gmail.com>
2026-04-17 13:52:41 +0700

    connect us02 and us03 by using map as entry for station detail page

---
+++ lib/ui/screens/map/widgets/station_marker.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:g4_t1_velo_app/model/station.dart';
+import 'package:g4_t1_velo_app/ui/theme/theme.dart';
+
+class StationMarker extends StatelessWidget {
+  const StationMarker({
+    super.key,
+    required this.station,
+    this.onTap,
+  });
+
+  final Station station;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final availableBikeCount = station.getAvailableSlots().length;
+    final pinColor = availableBikeCount > 0
+        ? appTheme.primaryColor
+        : AppColors.neutralLighter;
+
+    return GestureDetector(
+      onTap: onTap,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Icon(
+            Icons.location_on,
+            color: pinColor,
+            size: 72,
+          ),
+          Positioned(
+            top: 20,
+            child: Container(
+              width: 28,
+              height: 28,
+              alignment: Alignment.center,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 8,
+                    color: Color(0x26000000),
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Text(
+                '$availableBikeCount',
+                style: TextStyle(
+                  color: AppColors.neutralDark,
+                  fontSize: 12,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
